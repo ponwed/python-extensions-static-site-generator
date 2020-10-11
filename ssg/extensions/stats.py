3f470c5a78edabca84b6b86f5304a403ad2eae38
Pontus <pontus.wed@gmail.com>
2020-10-11 19:50:51 +0200

    Module 4

---
+++ ssg/extensions/stats.py
@@ -0,0 +1,24 @@
+import time 
+
+from ssg import hooks
+
+
+start_time = None
+total_written = 0
+
+@hooks.register("start_build")
+def start_build():
+    global start_time
+    start_time = time.time()
+
+@hooks.register("written")
+def written():
+    global total_written
+    total_written = total_written + 1
+
+@hooks.register("stats")
+def stats():
+    final_time = time.time() - start_time
+    average = final_time / total_written if total_written else 0
+    report = "Converted: {} \u00b7 Time: {:.2f} sec \u00b7 Avg: {:.4f} sec/file"
+    print(report.format(total_written, final_time, average))
