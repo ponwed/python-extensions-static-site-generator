c4160ae6f154cf01b0a22f09f7ac94be77a4c5c1
Pontus <pontus.wed@gmail.com>
2020-10-11 20:09:00 +0200

    Moved a parentheses, was cauing AttributeError

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -12,5 +12,5 @@ def load_directory(directory):
         load_module(directory.as_posix(), path.stem)
 
 def load_bundled():
-    directory = Path(__file__.parent/"extensions")
+    directory = Path(__file__).parent / "extensions"
     load_directory(directory)
